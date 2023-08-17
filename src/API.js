47ce32391683c1237efca496a013b7b136a1ef08
MaksymBora <maxboraod@gmail.com>
2023-08-17 16:59:25 +0200

    add id request

--- src/API.js
+++ src/API.js
@@ -4,11 +4,13 @@ const API_KEY = '20403084-138caa44d9b5066c1dd91e458';
 axios.defaults.baseURL = `https://pixabay.com/api/`;
 
 export const fetchImages = async (query, page) => {
+  const separated = query.split('/');
+  const exstractedQuery = separated[1];
   try {
     const response = await axios.get('', {
       params: {
         key: API_KEY,
-        q: query,
+        q: exstractedQuery,
         page: page,
         per_page: 30,
       },
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,29 +9,30 @@ export class App extends Component {
   };
 
   changeQuery = newQuery => {
-    // const queryId = `${Date.now()}/${newQuery}`;
-    // const query = queryId.split('/');
-    // const exstractedQuery = query[1];
-
     this.setState({
-      query: newQuery,
+      query: `${Date.now()}/${newQuery}`,
       images: [],
       page: 1
     });
+    
   };
 
   componentDidUpdate = async (prevProps, prevState) => {
-     if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
-         
-      
+    if (prevState.query !== this.state.query || prevState.page !== this.state.page) {
       try {
         const img = await fetchImages(this.state.query, this.state.page);
         this.setState({ images: img });
+        
       } catch (error) {
         console.log(error);
-       }
-     }
-  }
+      }
+    }
+  };
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    this.changeQuery(evt.target.elements.query.value);
+  };
 
   handleLoadMore = () => {
     this.setState(prevState => ({ page: prevState.page + 1 }));
@@ -42,12 +43,9 @@ export class App extends Component {
     return (
       <div>
         <div>
-          <form onSubmit={(evt) => {
-            evt.preventDefault();
-            this.changeQuery(evt.target.elements.query.value)
-          }}>
+          <form  onSubmit={this.handleSubmit}>
             <input type="text" name="query"/>
-            <button>Search</button>
+            <button type='submit'>Search</button>
           </form>
         </div>
         
